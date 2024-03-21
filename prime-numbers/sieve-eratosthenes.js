8a6b475b31bf1e7791cb65966984f0f8f9fb4932
khanhduy14 <tranhakhanhduy.dev@gmail.com>
2024-03-21 10:06:00 +0700

    prime-numbers: sieve-eratosthenes

---
+++ prime-numbers/sieve-eratosthenes.js
@@ -0,0 +1,22 @@
+const sieveEratosthenes = (n) => {
+    const markedPrimeList = Array.from({ length: n + 1 }, (_, __) => true)
+    const listPrime = []
+    for (let primeNum = 2; primeNum * primeNum <= n; primeNum++) {
+        if (markedPrimeList[primeNum]) {
+            for (let i = primeNum * primeNum; i <= n; i += primeNum) {
+                markedPrimeList[i] = false
+            }
+        }
+    }
+    for (let i = 2; i < markedPrimeList.length; i++) {
+        if (markedPrimeList[i]) {
+            listPrime.push(i)
+        }
+
+    }
+    return listPrime
+}
+
+
+const listPrimeLessThan100 = sieveEratosthenes(100)
+console.log(listPrimeLessThan100)
