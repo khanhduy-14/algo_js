dc41c498412d8ed97380392b5bb368a82ca3aa36
khanhduy14 <tranhakhanhduy.dev@gmail.com>
2024-04-14 15:37:36 +0700

    tree: add and remove in bst

--- tree/bst.js
+++ tree/bst.js
@@ -17,9 +17,67 @@ class BinarySearchTree {
         }
         return true
     }
+
+    static insert(root, val) {
+        if (root === null) {
+            return new BinarySearchTree(val)
+        }
+
+        if (val > root.val) {
+            root.right = this.insert(root.right, val)
+        }
+        else if (val < root.val) {
+            root.left = this.insert(root.left, val)
+        }
+        return root
+    }
+
+    static minValueNode(root) {
+        let cur = root
+        while (cur.left !== null) {
+            cur = cur.left
+        }
+        return cur
+    }
+
+    static remove(root, val) {
+        if (root === null) {
+            return root
+        }
+        if (val > root.val) {
+            root.right = this.remove(root.right, val)
+        }
+        else if (val < root.val) {
+            root.left = this.remove(root.left, val)
+        }
+        else {
+            if (root.left === null) {
+                return root.right
+            }
+            else if (root.right === null) {
+                return root.left
+            }
+            else {
+                const minValueNode = this.minValueNode(root.right)
+                root.val = minValueNode.val
+                root.right = this.remove(root.right, minValueNode.val)
+            }
+
+        }
+        return root
+
+    }
 }
 
-const tree = new BinarySearchTree(2, new BinarySearchTree(1, null, null), new BinarySearchTree(3, null, new BinarySearchTree(4, null, null)))
+const tree = new BinarySearchTree(4, null, null)
+BinarySearchTree.insert(tree, 3)
+BinarySearchTree.insert(tree, 2)
+BinarySearchTree.insert(tree, 6)
+BinarySearchTree.insert(tree, 5)
+BinarySearchTree.insert(tree, 7)
 
-console.log(BinarySearchTree.search(tree, 5))
-console.log(BinarySearchTree.search(tree, 1))
+BinarySearchTree.remove(tree, 7)
+
+console.log(tree);
+
+
